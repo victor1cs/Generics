0c83f5a3e429a43b10f0740d9042c6531352f3b4
Victor Correa <victor1cs@outlook.com>
2022-09-22 04:03:19 -0300

    Trabalhando com o TypeService, utilizado para melhorar a performance do codigo e nao deixar passar erros de compilacao

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,14 @@
+﻿using Generics;
+using System;
+
+PrintService<int> printService = new PrintService<int>();
+Console.WriteLine("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++) {
+    int x = int.Parse(Console.ReadLine());
+    printService.addValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
